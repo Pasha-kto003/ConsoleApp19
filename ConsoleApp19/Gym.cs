1421f2b735f1689d572dba3a8fc4824310684ca0
Pasha-kto003 <lukovnikov.pasha@gmail.com>
2020-11-17 11:36:17 +0300

    gym добавление

---
+++ ConsoleApp19/Gym.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp19
+{
+    class Gym
+    {
+
+        List<Animal> animals= new List<Animal>();
+
+        public void AddAnimal(Animal animal)
+        {
+            animals.Add(animal);
+        }
+        public void LetsMove()
+        {
+            for (int i = 0; i < animals.Count; i++)
+            {
+                if (animals[i] is IWalking)
+                {
+
+                    IWalking iDog = animals[i] as IWalking;
+                    iDog.Walk();
+                }
+
+                else if (animals[i] is ISwimming)
+
+                {
+                    ISwimming iFish = animals[i] as ISwimming;
+                    iFish.Swim();
+                }
+
+                else if (animals[i] is IFlying)
+                {
+                    IFlying iBird = animals[i] as IFlying;
+                    iBird.Fly();
+                }
+                else if (animals[i] is IFlying)
+                {
+                    IFlying iButterfly = animals[i] as IFlying;
+                    iButterfly.Fly();
+                }
+
+            }
+
+        }
+
+    }
+}
--- ConsoleApp19/Program.cs
+++ ConsoleApp19/Program.cs
@@ -6,20 +6,31 @@ namespace ConsoleApp19
     {
         static void Main(string[] args)
         {
-            ((IWalking)dog).Walk();
+            Dog dog = new Dog("Kabosu", new DateTime(2010, 8, 10)); // ada
+            Console.WriteLine($" Возраст Кабосу: { dog.Age} лет"); 
+            //((IWalking)dog).Walk(); //hm
 
             Butterfly but = new Butterfly("Machaon", new DateTime(2019, 10, 10));
             Console.WriteLine($"Возраст Махаона : { but.Age} год");
-            ((IFlying)but).Fly();
+            //((IFlying)but).Fly();
 
             Bird bidy = new Bird("Tanusha",  new DateTime( 2007, 7, 7));
             Console.WriteLine($"Возраст Танюши: {bidy.Age} лет");
-            ((IFlying)bidy).Fly();
+            //((IFlying)bidy).Fly();
 
             Fish fsh = new Fish("Nemo", new DateTime(2007, 7, 12));
             Console.WriteLine($"Возраст Немо: { fsh.Age} лет");
-            ((ISwimming)fsh).Swim();
+            //((ISwimming)fsh).Swim();
+
+            Gym gym = new Gym();
+            gym.AddAnimal(dog);
+            gym.AddAnimal(but);
+            gym.AddAnimal(bidy);
+            gym.AddAnimal(fsh);
+            gym.LetsMove();
+            Console.ReadKey();
         }
+
 
     }
 }
